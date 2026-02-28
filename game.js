23d2927f05e40bd20a9e2e417f359bb5be909882
Mihnea Radoi <mihnearadoi2010@gmail.com>
2026-02-28 13:39:12 +0200

    Revert "extracted to minimax.js"
    This reverts commit bbc6b0cfb61d065a01f8cc98f30cc586ee1876f7.

--- game.js
+++ game.js
@@ -18,44 +18,6 @@ Tie: 0
 let gameOver = false;
 let xTurn = true;
 
-// menu variables
-const gameOverScreen = document.querySelector('.game-over');
-const endMessage = document.querySelector('.end-message');
-const refreshButton = document.querySelector('.refresh-button');
-
-const settingsButton = document.querySelector('#setting-wheel');
-const settingsScreen = document.querySelector('.settings');
-
-const difficultyMenu = document.querySelector('.difficulty-menu');
-const difficultyButton = document.querySelector('.difficulty-settings');
-const difficulties = document.querySelectorAll('.difficulty');
-const difficultyDisplay = document.querySelector('.difficulty-display');
-
-const multiplayerButton = document.querySelector('.multiplayer-settings');
-const multiplayerMenu = document.querySelector('.multiplayer-menu');
-const playerNumberButtons = document.querySelectorAll('.players');
-const multiplayerDisplay = document.querySelector('.multiplayer-display');
-
-const backArrows = document.querySelectorAll('.back');
-
-let difficulty = localStorage.getItem("difficulty") || "Medium";
-let gameType = localStorage.getItem("gameType") || "Singleplayer";
-
-let isSettingOpen = false;
-start();
-
-function start() {
-    setGameType();
-
-    multiplayerDisplay.innerHTML = `Players: ${gameType}`;
-    difficultyDisplay.innerHTML = `Difficulty: ${difficulty}`;
-
-    if (gameType == "Multiplayer") {
-        difficultyDisplay.style.display = "none";
-    }
-}
-
-// game
 function gameLoopMultiplayer(e) {
     let cell = e.currentTarget;
 
@@ -116,7 +78,7 @@ function gameLoopBot(e) {
         xTurn = false;
         triggerGameOver();
 
-        if (!gameOver) {
+        if (checkWin() !== null) {
             setTimeout(aiMove ,Math.random() * 1000);
         }
     }
@@ -171,11 +133,11 @@ function aiMove() {
             for (let j = 0; j < 3; j++) {
                 if (board[i][j] == "") {
                     board[i][j] = "O";
-                    const evaluation = minimax(board, checkWin, score, true);
+                    let score = minimax(true);
                     board[i][j] = "";
 
-                    if (evaluation < bestScore) {
-                        bestScore = evaluation;
+                    if (score < bestScore) {
+                        bestScore = score;
                         move = {row: i, col: j}; // create object shorthand
                     }
                 }
@@ -186,7 +148,6 @@ function aiMove() {
     
 
     board[move.row][move.col] = "O";
-    console.log(board);
 
     const cell = document.querySelector(`.box[data-row="${move.row}"][data-col="${move.col}"]`)
     cell.innerHTML = '<p class="clicked">O</p>';
@@ -197,6 +158,52 @@ function aiMove() {
 
     xTurn = true;
     triggerGameOver();
+}
+
+function minimax(isMaximizer) {
+    let result = checkWin();
+    if (result !== null) {
+        return score[result];
+    }
+
+    let bestScore;
+    if (isMaximizer) {
+        bestScore = -Infinity;
+
+        // going through all possible board moves
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] == "") {
+                    board[i][j] = "X";
+
+                    let score = minimax(false);
+                    bestScore = Math.max(bestScore, score);
+
+                    board[i][j] = "";
+                }
+            }
+        }
+    }
+
+    else {
+        bestScore = Infinity;
+
+        // going through all possible board moves
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] == "") {
+                    board[i][j] = "O";
+
+                    let score = minimax(true);
+                    bestScore = Math.min(bestScore, score);
+
+                    board[i][j] = "";
+                }
+            }
+        }
+    }
+
+    return bestScore;
 }
 
 function triggerGameOver() {
@@ -274,126 +281,4 @@ function randomInt(min, max) {
     return number; 
 }
 
-// menu
-refreshButton.addEventListener("click", restartGame);
-settingsButton.addEventListener("click", openSettings);
-
-multiplayerButton.addEventListener("click", openMultiplayerSettings);
-difficultyButton.addEventListener("click", openDifficultySettings);
-
-backArrows.forEach(arrow => {
-    arrow.addEventListener("click", returnToMainSettings);
-});
-
-playerNumberButtons.forEach(button => {
-    button.addEventListener("click", (e) => setPlayerNumber(e));
-});
-
-difficulties.forEach(difficultyLevel => {
-    difficultyLevel.addEventListener("click", (e) => setDifficulty(e));
-});
-
-function restartGame() {
-    window.location.reload();
-}
-
-function openSettings() {
-    difficultyMenu.style.display = "none";
-    multiplayerMenu.style.display = "none";
-    
-    if (isSettingOpen) {
-        settingsScreen.style.display = "none";
-
-        if (gameOver) {
-            gameOverScreen.style.display = "flex";
-        }
-    }
-    else {
-        settingsScreen.style.display = "flex";
-        difficultyButton.style.display = "flex";
-        multiplayerButton.style.display = "flex";
-
-        if (gameOver) {
-            gameOverScreen.style.display = "none";
-        }
-    }
-
-    isSettingOpen = !isSettingOpen
-}
-
-function openDifficultySettings() {
-    if (gameType !== "Singleplayer") {
-        window.alert("This feature is only avalable for choosing the bot's difficulty in singleplayer");
-        return;
-    }
-
-    difficultyButton.style.display = "none";
-    multiplayerButton.style.display = "none";
-
-    difficultyMenu.style.display = "flex";
-}
-
-function openMultiplayerSettings() {
-    difficultyButton.style.display = "none";
-    multiplayerButton.style.display = "none";
-
-    multiplayerMenu.style.display = "flex";
-}
-
-function returnToMainSettings() {
-    difficultyMenu.style.display = "none";
-    multiplayerMenu.style.display = "none";
-
-    settingsScreen.style.display = "flex";
-    difficultyButton.style.display = "flex";
-    multiplayerButton.style.display = "flex";
-}
-
-function setPlayerNumber(e) {
-    let playerNumber = e.currentTarget;
-    gameType = playerNumber.dataset.players;
-
-    localStorage.setItem("gameType", gameType);
-
-    multiplayerDisplay.innerHTML = `Players: ${gameType}`;
-
-    setGameType();
-}
-
-function setDifficulty(e) {
-    let level = e.currentTarget;
-    difficulty = level.dataset.difficulty;
-
-    localStorage.setItem("difficulty", difficulty);
-    console.log(localStorage.getItem("difficulty"));
-
-    difficultyDisplay.innerHTML = `Difficulty: ${difficulty}`;
-}
-
-function setGameType() {
-    if (gameType === "Singleplayer") {
-        cells.forEach(cell => {
-            cell.removeEventListener("click", gameLoopBot);
-            cell.removeEventListener("click", gameLoopMultiplayer);
-        });
-
-        cells.forEach(cell => {
-            cell.addEventListener("click", gameLoopBot);
-        });
-
-        difficultyDisplay.style.display = "flex";
-    }
-    else {
-        cells.forEach(cell => {
-            cell.removeEventListener("click", gameLoopBot);
-            cell.removeEventListener("click", gameLoopMultiplayer);
-        });
-
-        cells.forEach(cell => {
-            cell.addEventListener("click", gameLoopMultiplayer);
-        });
-
-        difficultyDisplay.style.display = "none";
-    }
-    
-}
+export {gameLoopMultiplayer, gameLoopBot};
